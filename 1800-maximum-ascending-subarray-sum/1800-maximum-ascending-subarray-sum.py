c71ba883f5857eb8de5ebf8f48f7f7579d0c47b7
Mohammad Kaif Iqbal <133042453+mkaifiqbal@users.noreply.github.com>
2025-02-04 18:41:43 +0530

    Time: 0 ms (100%), Space: 17.8 MB (44.25%) - LeetHub

--- 1800-maximum-ascending-subarray-sum/1800-maximum-ascending-subarray-sum.py
+++ 1800-maximum-ascending-subarray-sum/1800-maximum-ascending-subarray-sum.py
@@ -1,9 +1,9 @@
 class Solution:
     def maxAscendingSum(self, nums: List[int]) -> int:
-        
+        n = len(nums)
         maxsum =0
         sum1=nums[0]
-        for i in range(1,len(nums)):
+        for i in range(1,n):
             if nums[i-1] < nums[i]:
                 sum1 += nums[i]
             else :
