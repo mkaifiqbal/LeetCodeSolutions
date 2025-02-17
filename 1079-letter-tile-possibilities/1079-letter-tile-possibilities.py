f7c393cce1c0a8a0fa0c8cfd70983389d1b409cb
Mohammad Kaif Iqbal <133042453+mkaifiqbal@users.noreply.github.com>
2025-02-17 10:21:32 +0530

    Time: 19 ms (88.75%), Space: 19.9 MB (24.93%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.py
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.py
@@ -3,13 +3,15 @@ class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
         n = len(tiles)
         l1=[]
+        count=0
         for i in tiles:
             l1.append(i)
         all_passwords = []
         for r in range(1, n + 1):
-            perms = set(permutations(l1, r))  
-            all_passwords.extend(perms)
+            perms = set(permutations(l1, r)) 
+            count = count+len(perms) 
+            #all_passwords.extend(perms)
 
-        return len(all_passwords)
+        return count
         
         
