66ec9fa8f960db73f5dda536f071081ad0a6be66
Mohammad Kaif Iqbal <133042453+mkaifiqbal@users.noreply.github.com>
2025-10-27 01:40:25 +0530

    Time: 10 ms (23.88%), Space: 10 MB (44.4%) - LeetHub

--- 3461-check-if-digits-are-equal-in-string-after-operations-i/3461-check-if-digits-are-equal-in-string-after-operations-i.cpp
+++ 3461-check-if-digits-are-equal-in-string-after-operations-i/3461-check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
     bool hasSameDigits(string s) {
-        string temp =s;
-        string newstr="";
-        int n=s.size();
-        while(n!=2){
-            newstr="";
-            for (int i=1;i<n;i++){
-                int sum= (temp[i-1]-'0') + (temp[i]-'0');
-                newstr += to_string(sum%10);
-            }
-            temp = newstr;
-            n = temp.size();            
+        // string temp =s;
+        // string newstr="";
+        // int n=s.size();
+        // while(n!=2){
+        //     newstr="";
+        //     for (int i=1;i<n;i++){
+        //         int sum= (temp[i-1]-'0') + (temp[i]-'0');
+        //         newstr += to_string(sum%10);
+        //     }
+        //     temp = newstr;
+        //     n = temp.size();            
 
+        // }
+        // return (newstr[0] == newstr[1]);
+        while (s.size() > 2) {
+        string t = "";
+        for (int i = 1; i < s.size(); i++) {
+            t += char(((s[i-1] - '0') + (s[i] - '0')) % 10 + '0');
         }
-        return (newstr[0] == newstr[1]);
+        s = t;
+    }
+    return s[0] == s[1];
         
     }
 };
