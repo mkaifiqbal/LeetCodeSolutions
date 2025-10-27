fc0dcc0f28e672d8527b409a99772943205dd523
Mohammad Kaif Iqbal <133042453+mkaifiqbal@users.noreply.github.com>
2025-10-27 16:05:52 +0530

    Time: 5 ms (66.1%), Space: 27.4 MB (68.24%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -3,14 +3,12 @@ public:
     int numberOfBeams(vector<string>& bank) {
         int beam =0;
         int prelaser=0;
-        // int n= bank.size();
-        // int m = bank[0].size();
-        // for(int i=0;i<n;i++){
-        for (string s: bank){
+        int n= bank.size();
+        int m = bank[0].size();
+        for(int i=0;i<n;i++){
             int laser =0;
-            // for(int j=0;j<m;j++){
-            for(char c: s){
-                if (c=='1'){
+            for(int j=0;j<m;j++){
+                if (bank[i][j]=='1'){
                     laser++;
                 }
             }
