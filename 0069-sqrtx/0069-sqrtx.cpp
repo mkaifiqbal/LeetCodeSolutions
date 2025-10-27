894407ad8dc61f197afb96fb0a61ae643bb1423f
Mohammad Kaif Iqbal <133042453+mkaifiqbal@users.noreply.github.com>
2025-10-27 12:36:04 +0530

    Time: 0 ms (100%), Space: 8.6 MB (50.51%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low =0;
+        int high =x;
+
+        while(low<=high){
+            long long int mid = (low+high)/2;
+            if(mid*mid<=x){
+                low = mid+1;
+            }else high = mid-1;
+        }return high;
+    }
+};
