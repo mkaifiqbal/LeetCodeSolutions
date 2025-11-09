54925b16a36236edf755cbc308d507c8ee128dcb
Mohammad Kaif Iqbal <133042453+mkaifiqbal@users.noreply.github.com>
2025-11-09 12:55:54 +0530

    Time: 0 ms (100%), Space: 8 MB (40.22%) - LeetHub

--- 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int countOperations(int num1, int num2) {
-        long long count =0;
-        while(true){
-            if(num1==0 || num2==0) return count;
+        int count =0;
+        while(num1!=0 && num2!=0){
             if(num1 > num2){
                 num1-=num2;
                 count++;
@@ -12,6 +11,6 @@ public:
                 count++;
             }
         }
-        
+        return count;
     }
 };
