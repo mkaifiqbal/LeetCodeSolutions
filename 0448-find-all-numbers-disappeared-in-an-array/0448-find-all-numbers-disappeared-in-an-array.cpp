77f94c395b0b77bb337f055ee9ce74ac33b1ee05
Mohammad Kaif Iqbal <133042453+mkaifiqbal@users.noreply.github.com>
2025-11-21 00:31:14 +0530

    Time: 10 ms (39.98%), Space: 54.9 MB (41.09%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -5,7 +5,7 @@ public:
         vector<int> arr(n+1,0);
         vector<int> ans;
         for(int i=0;i<n;i++){
-            arr[nums[i]] +=1;
+            arr[nums[i]]++;
         }
         for(int j=1;j<n+1;j++){
             if(arr[j]==0) ans.push_back(j);
