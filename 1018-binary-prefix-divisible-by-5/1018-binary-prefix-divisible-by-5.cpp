deeb819ef619e69c83d32e91d6121ff00907cea7
Mohammad Kaif Iqbal <133042453+mkaifiqbal@users.noreply.github.com>
2025-11-24 22:02:17 +0530

    Update 1018-binary-prefix-divisible-by-5.cpp
    optiml

--- 1018-binary-prefix-divisible-by-5/1018-binary-prefix-divisible-by-5.cpp
+++ 1018-binary-prefix-divisible-by-5/1018-binary-prefix-divisible-by-5.cpp
@@ -1,20 +1,36 @@
 class Solution {
 public:
-    bool div(string a){
-        int state=0;
-        for(char i:a){
-            int bit = i-'0';
-            state=(state*2 +bit)%5;
-        }
-        return state ==0;
-    }
+
     vector<bool> prefixesDivBy5(vector<int>& nums) {
         vector<bool> ans;
-        string a="";
+        int state=0;
         for(int i=0;i<nums.size();i++){
-            a+= to_string(nums[i]);
-            ans.push_back(div(a));
+            state=(state*2 +nums[i])%5;
+            ans.push_back(state==0);
         }
+        
         return ans;
     }
 };
+
+
+// class Solution {
+// public:
+//     bool div(string a){
+//         int state=0;
+//         for(char i:a){
+//             int bit = i-'0';
+//             state=(state*2 +bit)%5;
+//         }
+//         return state ==0;
+//     }
+//     vector<bool> prefixesDivBy5(vector<int>& nums) {
+//         vector<bool> ans;
+//         string a="";
+//         for(int i=0;i<nums.size();i++){
+//             a+= to_string(nums[i]);
+//             ans.push_back(div(a));
+//         }
+//         return ans;
+//     }
+// };
