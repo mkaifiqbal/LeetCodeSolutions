f59617e542e964c5917b8c0a565ddb8df1f0e89d
Mohammad Kaif Iqbal <133042453+mkaifiqbal@users.noreply.github.com>
2025-12-08 12:50:34 +0530

    Time: 56 ms (31.06%), Space: 90.8 MB (44.77%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> count;
+        for(int i=0;i< nums.size();i++){
+            if(count.find(nums[i])!=count.end()) return true;
+            count[nums[i]] =1;
+        }
+        return false;
+    }
+};
