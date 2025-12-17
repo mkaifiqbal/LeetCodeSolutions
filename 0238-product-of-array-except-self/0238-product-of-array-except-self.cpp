2442f0084315198858f54ce2b4e6f1d8aa579777
Mohammad Kaif Iqbal <133042453+mkaifiqbal@users.noreply.github.com>
2025-12-18 00:07:23 +0530

    Time: 3 ms (39.16%), Space: 40.1 MB (94.15%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,25 +2,47 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n= nums.size();
-        vector<int> ans(n);
-        vector<long long> left;
-        vector<long long> right(n);
-        long long mul1 =1;
-        long long mul2=1;
+        vector<int> ans(n,1);
+
+        long long prefix=1;
+
         for(int i=0;i<n;i++){
-            mul1 *=nums[i];
-            mul2 *=nums[n-i-1];
-            left.push_back(mul1);
-            right[n-i-1]=mul2;
+            ans[i] =prefix;
+            prefix *= nums[i];
+        }
+
+        long long sufix=1;
+
+        for(int i=n-1;i>=0;i--){
+            ans[i] *= sufix;
+            sufix *= nums[i];
 
         }
-        for(int i=0;i<n;i++){
-            if(i==0) ans[i]=right[1];
-            else if(i==n-1) ans[i]=left[i-1];
-            else ans[i]=left[i-1]*right[i+1];
-        }
-        
         return ans;
+
+
+
+
+        // int n= nums.size();
+        // vector<int> ans(n);
+        // vector<long long> left;
+        // vector<long long> right(n);
+        // long long mul1 =1;
+        // long long mul2=1;
+        // for(int i=0;i<n;i++){
+        //     mul1 *=nums[i];
+        //     mul2 *=nums[n-i-1];
+        //     left.push_back(mul1);
+        //     right[n-i-1]=mul2;
+
+        // }
+        // for(int i=0;i<n;i++){
+        //     if(i==0) ans[i]=right[1];
+        //     else if(i==n-1) ans[i]=left[i-1];
+        //     else ans[i]=left[i-1]*right[i+1];
+        // }
+        
+        // return ans;
 
     }
 };
