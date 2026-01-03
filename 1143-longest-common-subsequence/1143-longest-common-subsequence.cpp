795a9bc86c6e4ae2572a68a1ac7b561885e35df0
Mohammad Kaif Iqbal <133042453+mkaifiqbal@users.noreply.github.com>
2026-01-03 06:12:31 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int solve(int indx1, int indx2, string s1, string s2,vector<vector<int>>& dp){
+        if(indx1<0 || indx2 <0){
+            return 0;
+        }
+        if(dp[indx1][indx2]!=-1){
+            return dp[indx1][indx2];
+        }
+        if(s1[indx1]==s2[indx2] ){
+            return dp[indx1][indx2]=solve(indx1-1, indx2-1, s1,s2,dp)+1;
+        }
+        return dp[indx1][indx2]=max(solve(indx1-1,indx2,s1,s2,dp), solve(indx1,indx2-1,s1,s2,dp));
+    }
+
+    int longestCommonSubsequence(string text1, string text2) {
+        int indx1= text1.size()-1;
+        int indx2= text2.size()-1;
+        vector<vector<int>> dp(indx1+2,vector<int>(indx2+2,-1));
+        return solve(indx1,indx2,text1,text2,dp);
+    }
+};
