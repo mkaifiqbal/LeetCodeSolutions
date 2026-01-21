9824dd04edaca7b0053aafa73a7a389d294f5791
Mohammad Kaif Iqbal <133042453+mkaifiqbal@users.noreply.github.com>
2026-01-21 03:23:00 -0600

    Time: 0 ms (100%), Space: 40.1 MB (79.29%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -3,25 +3,23 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n= nums.size();
         vector<int> ans(n,1);
-
-        long long prefix=1;
-
-        for(int i=0;i<n;i++){
-            ans[i] =prefix;
+        int prefix=1;
+        for(int i=0; i<n;i++){
+            ans[i]= prefix;
             prefix *= nums[i];
         }
 
-        long long sufix=1;
-
+        int suf=1;
         for(int i=n-1;i>=0;i--){
-            ans[i] *= sufix;
-            sufix *= nums[i];
+            ans[i] *= suf;
+            suf *= nums[i]; 
 
         }
+
         return ans;
 
 
-
+        
 
         // int n= nums.size();
         // vector<int> ans(n);
