dbfa33a738b4a2dd01d5bf630ff2dd541f4938d2
Mohammad Kaif Iqbal <133042453+mkaifiqbal@users.noreply.github.com>
2026-01-23 02:48:38 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -3,14 +3,33 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n=matrix.size();
         int m = matrix[0].size();
-        int l=0;
-        int r=m-1;
-        while(l < n && r >= 0){
-            if(matrix[l][r]==target) return true;
-            if(matrix[l][r]>target){
-                r--;
-            }else l++;
+
+        int s=0;
+        int end= n*m-1;
+
+        while(s<=end){
+            int mid= (s+end)/2;
+            if(matrix[mid/n][mid%m]== target) return true;
+            if(matrix[mid/n][mid%m]>target){
+                end= mid-1;
+            }else{
+                s=mid+1;
+            }
         }
         return false;
+
+
+
+        // int n=matrix.size();
+        // int m = matrix[0].size();
+        // int l=0;
+        // int r=m-1;
+        // while(l < n && r >= 0){
+        //     if(matrix[l][r]==target) return true;
+        //     if(matrix[l][r]>target){
+        //         r--;
+        //     }else l++;
+        // }
+        // return false;
     }
 };
