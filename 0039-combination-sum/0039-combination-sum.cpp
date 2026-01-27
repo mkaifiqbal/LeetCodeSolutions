d77fd48f34163fa7e0a6fb596ea896a5ab13f2b5
Mohammad Kaif Iqbal <133042453+mkaifiqbal@users.noreply.github.com>
2026-01-27 01:49:42 -0600

    Time: 4 ms (40%), Space: 14.1 MB (75.6%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     vector<int> current;
-    void solve(int indx,vector<int>& arr, int target, vector<vector<int>> &ans){
-        if(target==0){
+    void solve(int indx, vector<int>& arr, int target,
+               vector<vector<int>>& ans) {
+        if (target == 0) {
             ans.push_back(current);
             return;
         }
-        if(target<0) return ;
-        for(int i=indx;i<arr.size();i++){
+        if (target < 0)
+            return;
+        for (int i = indx; i < arr.size(); i++) {
             current.push_back(arr[i]);
-            solve(i, arr,target - arr[i], ans); 
+            solve(i, arr, target - arr[i], ans);
             current.pop_back();
         }
     }
     vector<vector<int>> combinationSum(vector<int>& arr, int target) {
         vector<vector<int>> ans;
-        solve(0,arr,target,ans);
+        solve(0, arr, target, ans);
         return ans;
     }
 };
