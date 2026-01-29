c82924aaaa90deefe1d8f252de652a7ba7d693f3
Mohammad Kaif Iqbal <133042453+mkaifiqbal@users.noreply.github.com>
2026-01-29 02:21:27 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        vector<int> ans;
-        for(int i=1;i<=n;i++){
-            ans.push_back(i);
-        }
+        // vector<int> ans;
+        // for(int i=1;i<=n;i++){
+        //     ans.push_back(i);
+        // }
+        // int indx=0;
+        // while(ans.size()>1){
+        //     indx = (indx+k-1) %ans.size();
+        //     ans.erase(ans.begin()+indx);    
+        // }
+        // return ans[0];
         int indx=0;
-        while(ans.size()>1){
-            indx = (indx+k-1) %ans.size();
-            ans.erase(ans.begin()+indx);    
+        for(int i=0;i<n;i++){
+            indx= (indx+k)%(n-1);
         }
-        return ans[0];
+        return indx+1;
     }
 };
