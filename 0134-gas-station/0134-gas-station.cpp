7297445267eb6d6a1b471773379ad0b59fd74133
Mohammad Kaif Iqbal <133042453+mkaifiqbal@users.noreply.github.com>
2026-02-01 22:46:09 -0600

    Time: 3 ms (36.13%), Space: 112.4 MB (59.47%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int r = 0;
+        int f = 0;
+        int n = gas.size();
+        int balance = 0;
+        int gassum=0;
+        int costsum=0;
+        for (int i = 0; i < n; i++) {
+            balance += gas[i] - cost[i];
+            gassum += gas[i];
+            costsum += cost[i];
+            if (balance < 0) {
+                r = i + 1;
+                balance = 0;
+            }
+        }
+        if(gassum<costsum) return -1;
+        return r % n;
+    }
+};
