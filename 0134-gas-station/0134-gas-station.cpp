78876ecf94dbce361b7bd3f86245b3fbcca32395
Mohammad Kaif Iqbal <133042453+mkaifiqbal@users.noreply.github.com>
2026-03-03 23:23:38 +0530

    Time: 0 ms (100%), Space: 112.4 MB (60.54%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -4,18 +4,16 @@ public:
         int r = 0;
         int n = gas.size();
         int balance = 0;
-        int gassum=0;
-        int costsum=0;
+        int total=0;
         for (int i = 0; i < n; i++) {
             balance += gas[i] - cost[i];
-            gassum += gas[i];
-            costsum += cost[i];
+            total += gas[i] - cost[i];
             if (balance < 0) {
                 r = i + 1;
                 balance = 0;
             }
         }
-        if(gassum<costsum) return -1;
-        return r % n;
+        if(total<0) return -1;
+        return r;
     }
 };
