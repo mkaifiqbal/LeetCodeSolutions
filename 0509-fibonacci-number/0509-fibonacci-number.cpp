a58f06e5cdc3b350782cf68928312101ee669337
Mohammad Kaif Iqbal <133042453+mkaifiqbal@users.noreply.github.com>
2026-03-11 21:38:56 +0530

    Time: 2302 ms (23.19%), Space: 364.8 MB (10.55%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int solve(int n, vector<int> &dp){
+        if(n<=1)return n;
+        if(dp[n]!= -1) return dp[n];
+        return dp[n]=fib(n-1)+ fib(n-2);
+    }
+    int fib(int n) {
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
+    }
+};
