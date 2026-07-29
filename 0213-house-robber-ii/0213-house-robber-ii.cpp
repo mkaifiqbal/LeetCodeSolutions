952064d3c63a33e827e735babac0aa850a5d5183
Mohammad Kaif Iqbal <133042453+mkaifiqbal@users.noreply.github.com>
2026-07-29 11:38:45 +0530

    Time: 0 ms (100%), Space: 11 MB (7.67%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
-    int solve(int indx,int end,vector<int>&nums,vector<int> &dp){
-        if(indx>end){
-            return 0;
-        }
-        if(dp[indx]!=-1) return dp[indx];
-        int nt = solve(indx+1,end,nums,dp);
-        int take =0;
-        take = nums[indx]+solve(indx+2,end,nums,dp);
-        return dp[indx]= max(nt,take);
+    int solve(int indx,int end, vector<int>& nums,vector<int>&dp){
+        if(indx>= end) return 0;
+        if(dp[indx]!= -1) return dp[indx];
+        int pick= nums[indx]+ solve(indx+2,end,nums,dp);
+        int npick= solve(indx+1,end,nums,dp);
+        return dp[indx]=max(pick,npick);
     }
     int rob(vector<int>& nums) {
         int n= nums.size();
-        vector<int> dp(n+1,-1);
-        vector<int> dp1(n+1,-1);
-        if(n==1) return nums[0];
-
-        int a= solve(1,n-1,nums,dp);
-        int b = solve(0,n-2,nums,dp1);
-        return max(a,b);
+        if(n==1)return nums[0];
+        vector<int> dp(n+1,-1),dp1(n+1,-1);
+        int pick= solve(0,n-1,nums,dp);
+        int npick = solve(1,n,nums,dp1);
+        return max(pick,npick);
     }
 };
